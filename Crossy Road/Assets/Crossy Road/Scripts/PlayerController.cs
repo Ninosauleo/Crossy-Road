2392a3998adcda1d92e42dab3918ae5a0a08e3fe
Ninosauleo <ninosauleo@gmail.com>
2017-02-28 11:56:59 +0100

    Character moving

--- Crossy Road/Assets/Crossy Road/Scripts/PlayerController.cs
+++ Crossy Road/Assets/Crossy Road/Scripts/PlayerController.cs
@@ -16,4 +16,59 @@ public class PlayerController : MonoBehaviour {
 	private Renderer renderer = null;
 	private bool isVisible = false;
 
+	void Start(){
+	
+	}
+
+	void Update(){
+		CanMove ();
+
+	}
+		
+	void CanIdle(){
+	
+	}
+
+	void CheckIfCanmove(){
+	
+	}
+
+	void setMove(){
+	
+	}
+
+	void CanMove(){
+		if(Input.GetKeyDown (KeyCode.UpArrow)){
+			Moving (new Vector3(transform.position.x, transform.position.y, transform.position.z + moveDistance));
+		}
+		else if(Input.GetKeyDown (KeyCode.DownArrow)){
+			Moving (new Vector3(transform.position.x, transform.position.y, transform.position.z - moveDistance));
+		}
+		else if(Input.GetKeyDown (KeyCode.LeftArrow)){
+			Moving (new Vector3(transform.position.x - moveDistance, transform.position.y, transform.position.z));
+		}
+		 else if(Input.GetKeyDown (KeyCode.RightArrow)){
+			Moving (new Vector3(transform.position.x + moveDistance, transform.position.y, transform.position.z + moveDistance));
+		}
+	}
+		
+	void Moving(Vector3 pos){
+		LeanTween.move (this.gameObject, pos, moveTime);
+	}
+
+	void MoveComplete(){
+	
+	}
+
+	void SetMoveForwardState(){
+	
+	}
+
+	void IsVisible(){
+	
+	}
+
+	void GotHit(){
+	
+	}
 }
